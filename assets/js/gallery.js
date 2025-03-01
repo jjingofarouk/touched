19b5e61beae33d83ab4d73e702bd65fe7c760538
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-01 14:20:56 +0300

    Update gallery.js

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,19 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".filter-btn");
+    const programCards = document.querySelectorAll(".program-card");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function () {
+            const category = this.getAttribute("data-category");
+
+            programCards.forEach(card => {
+                if (category === "all" || card.getAttribute("data-category") === category) {
+                    card.style.display = "block";
+                } else {
+                    card.style.display = "none";
+                }
+            });
+        });
+    });
+});
