d2014ce5df4e859dd708c6ebca8567767acc55ba
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-15 16:31:33 -0700

    Update Navbar.js

--- touchedhearts/src/components/Navbar.js
+++ touchedhearts/src/components/Navbar.js
@@ -1,86 +1,56 @@
 // src/components/Navbar.js
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../assets/images/logo.jpg'; // Adjust path as needed
 import '../styles/Navbar.css';
 
-const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+const CustomNavLink = ({ to, children, ...props }) => (
+  <Nav.Link as={NavLink} to={to} activeClassName="active" {...props}>
+    {children}
+  </Nav.Link>
+);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
+const CustomDropdownItem = ({ to, children }) => (
+  <NavDropdown.Item as={NavLink} to={to} activeClassName="active">
+    {children}
+  </NavDropdown.Item>
+);
 
+const NavbarComponent = () => {
   return (
     <header>
-      <nav className="navbar">
-        <div className="nav-container">
-          <NavLink to="/" className="nav-logo">
+      <Navbar expand="lg" className="navbar-custom">
+        <Container>
+          <Navbar.Brand as={NavLink} to="/" className="nav-logo">
             <img src={logo} alt="Touched Hearts Logo" className="logo" />
-          </NavLink>
-          <button
-            className={`nav-toggle ${isMenuOpen ? 'active' : ''}`}
-            aria-label="Toggle navigation"
-            onClick={toggleMenu}
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-          <ul className={`nav-menu ${isMenuOpen ? 'active' : ''}`}>
-            <li>
-              <NavLink to="/" className="nav-link" activeClassName="active" exact>
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" className="nav-link" activeClassName="active">
-                About Us
-              </NavLink>
-            </li>
-            <li className="nav-dropdown">
-              <NavLink to="/programs" className="nav-link" activeClassName="active">
-                Programs <i className="fas fa-chevron-down"></i>
-              </NavLink>
-              <ul className="dropdown-menu">
-                <li>
-                  <NavLink to="/education">Education</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/healthcare">Healthcare</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/disabilities">Disability Support</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/community">Community Development</NavLink>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <NavLink to="/stories" className="nav-link" activeClassName="active">
-                Stories
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/gallery" className="nav-link" activeClassName="active">
-                Gallery
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/get-involved" className="nav-link" activeClassName="active">
-                Get Involved 
-              </NavLink>
-             
-            </li>
-            <li>
-              <NavLink to="/donate" className="nav-link btn-donate" activeClassName="active">
-                Donate Now
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="nav-toggle" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto nav-menu">
+              <CustomNavLink to="/" exact>Home</CustomNavLink>
+              <CustomNavLink to="/about">About Us</CustomNavLink>
+              <NavDropdown
+                title={<>Programs <i className="fas fa-chevron-down"></i></>}
+                id="programs-dropdown"
+                className="nav-dropdown"
+              >
+                <CustomDropdownItem to="/education">Education</CustomDropdownItem>
+                <CustomDropdownItem to="/healthcare">Healthcare</CustomDropdownItem>
+                <CustomDropdownItem to="/disabilities">Disability Support</CustomDropdownItem>
+                <CustomDropdownItem to="/community">Community Development</CustomDropdownItem>
+              </NavDropdown>
+              <CustomNavLink to="/stories">Stories</CustomNavLink>
+              <CustomNavLink to="/gallery">Gallery</CustomNavLink>
+              <CustomNavLink to="/get-involved">Get Involved</CustomNavLink>
+              <CustomNavLink to="/donate" className="btn-donate">Donate Now</CustomNavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };
