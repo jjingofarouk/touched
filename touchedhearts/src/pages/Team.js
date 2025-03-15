e3b7824d7adbe688eb956e73cbee3d0268a9b52b
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-15 04:06:29 -0700

    Update Team.js

---
+++ touchedhearts/src/pages/Team.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import TeamCard from "../components/TeamCard";
+import teamData from "../data/team.json"; // Team member data
+
+const Team = () => {
+  return (
+    <div className="team-page">
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Team Section */}
+      <main>
+        <section className="team-grid" aria-label="Meet Our Dedicated Team">
+          <h1 className="section-title">Meet Our Team</h1>
+          <div className="team-cards" role="list">
+            {teamData.map((member, index) => (
+              <TeamCard key={index} member={member} />
+            ))}
+          </div>
+        </section>
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Team;
