adeeec6a166b00622fb1da9cb7f770534f551f0f
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-15 01:37:42 -0700

    Update ImpactCards.js

---
+++ touchedhearts/src/components/ImpactCards.js
@@ -0,0 +1,26 @@
+// src/components/ImpactCards.js
+import React from 'react';
+
+const ImpactCards = () => {
+  const impactData = [
+    { number: '10,000+', text: 'Lives Touched' },
+    { number: '25', text: 'Communities Served' },
+    { number: '15', text: 'Years of Service' },
+    { number: '90%', text: 'Funds to Programs' },
+  ];
+
+  return (
+    <section className="impact-numbers">
+      <div className="impact-container">
+        {impactData.map((item, index) => (
+          <div className="impact-item" key={index}>
+            <span className="impact-number">{item.number}</span>
+            <p>{item.text}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ImpactCards;
