602ab979fa5bdee8c541e87235f4a8dcb7b7c42f
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-15 14:07:18 +0300

    Create TeamCard.js

---
+++ touchedhearts/src/components/TeamCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TeamCard = ({ member }) => {
+  return (
+    <div className="team-card" role="listitem">
+      <img src={member.image} alt={member.name} className="team-img" />
+      <div className="team-info">
+        <h2 className="team-name">{member.name}</h2>
+        <p className="role">{member.role}</p>
+        <p className="bio">{member.bio}</p>
+      </div>
+    </div>
+  );
+};
+
+export default TeamCard;
