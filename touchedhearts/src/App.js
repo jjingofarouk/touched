486202acc3d3d9b9278b07027e9ca4d138526642
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-17 09:46:15 -0700

    Update App.js

--- touchedhearts/src/App.js
+++ touchedhearts/src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-
+  import "bootstrap/dist/css/bootstrap.min.css";
+  
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
