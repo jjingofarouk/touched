cd4abc7ec74edf78e7d2d1c74fd52bcd27534299
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-17 09:34:49 -0700

    Update Navbar.js

--- touchedhearts/src/components/Navbar.js
+++ touchedhearts/src/components/Navbar.js
@@ -1,34 +1,63 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../assets/images/logo.jpg';
 
-function BasicExample() {
+const Navbar = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={NavLink} to="/">
+          <img
+            src={logo}
+            alt="Touched Hearts Logo"
+            height="30" // Bootstrap default logo height
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+            <Nav.Link as={NavLink} to="/" exact activeClassName="active">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/about" activeClassName="active">
+              About Us
+            </Nav.Link>
+            <NavDropdown title="Programs" id="programs-nav-dropdown">
+              <NavDropdown.Item as={NavLink} to="/education">
+                Education
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item as={NavLink} to="/healthcare">
+                Healthcare
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/disabilities">
+                Disability Support
+              </NavDropdown.Item>
+              <NavDropdown.Item as={NavLink} to="/community">
+                Community Development
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link as={NavLink} to="/stories" activeClassName="active">
+              Stories
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/gallery" activeClassName="active">
+              Gallery
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/get-involved" activeClassName="active">
+              Get Involved
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/donate" activeClassName="active">
+              Donate Now
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
-export default BasicExample;
+export default Navbar;
