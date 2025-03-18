50485fa74707ec89b02cd039efa8b9154b15c314
jjingofarouk <jjingofaroukk@gmail.com>
2025-03-18 02:18:12 -0700

    Update Donate.js

--- touchedhearts/src/pages/Donate.js
+++ touchedhearts/src/pages/Donate.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../styles/Donation.css'
 
 const Donate = () => {
   const navigate = useNavigate();
