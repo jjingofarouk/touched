768ad619ac165bfe8024ed0748c312082ca6ef2b
jjingofarouk <jjingofaroukk@gmail.com>
2025-04-22 16:57:51 +0000

    g

--- touchedhearts/src/pages/AnnualReports.js
+++ touchedhearts/src/pages/AnnualReports.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import headerImage from './reports-header.png'; // Import the header image
 
@@ -245,7 +244,6 @@ const AnnualReports = () => {
         </div>
       </section>
 
-      <Footer />
     </div>
   );
 };
